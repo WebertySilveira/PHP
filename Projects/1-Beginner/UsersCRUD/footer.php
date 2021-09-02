5e15ff1725ed4c417f219dc8edf09c00e8db6bac
webertysilveira <weberty.silveira@indexdigital.com.br>
2021-09-01 21:59:47 -0300

    Criar CRUD de Usuários

---
+++ Projects/1-Beginner/UsersCRUD/footer.php
@@ -0,0 +1,8 @@
+<footer class="page-footer font-small blue">
+    <div class="footer-copyright text-center py-3">
+        © <?php echo date("Y"); ?> Copyright:
+        <a href="https://github.com/WebertySilveira/PHP"> WebertySilveira</a>
+    </div>
+    </footer>
+</body>
+</html>
