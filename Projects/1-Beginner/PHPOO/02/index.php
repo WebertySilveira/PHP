42bb2a361e03c4c92be2d8f447f826ed65d0b838
Weberty Silveira <webertysilveira>
2021-12-15 16:31:09 -0300

    Challenge 02

---
+++ Projects/1-Beginner/PHPOO/02/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "Type.php";
+require_once "Product.php";
+
+$type[0] = new Type();
+$type[0]->setCode(1);
+$type[0]->setPercent(10);
+
+$type[1] = new Type();
+$type[1]->setCode(2);
+$type[1]->setPercent(20);
+
+$average[1] = 0;
+for ($i=1; $i <= 5; $i++) 
+{ 
+    $product[$i] = new Product();
+    $product[$i]->setDescription("Product {$i}");
+    $product[$i]->setPrice(50);
+    $product[$i]->setType($type[0]);
+    echo("{$product[$i]->getDescription()} - {$product[$i]->finalPrice()} </br>");
+
+    $average[$i] = $product[$i]->finalPrice();
+}
+
+$average = array_sum($average)/count($average);
+echo($average);
